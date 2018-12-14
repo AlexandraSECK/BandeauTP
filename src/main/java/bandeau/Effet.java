29ecc8220a1aa08cdd4054d9e7a3bf1fad27db79
AlexandraSECK <35805221+AlexandraSECK@users.noreply.github.com>
2018-12-14 09:29:53 +0100

    Commit

---
+++ src/main/java/bandeau/Effet.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bandeau;
+
+/**
+ *
+ * @author seck
+ */
+public abstract class Effet {
+    String texte;
+    
+    Effet(String t){
+        texte=t;
+    }
+    abstract void afficheToiSur(Bandeau b);
+}
